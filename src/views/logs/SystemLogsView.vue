8e23445e0d9456e4409b598a267ed9643f9ac478
cheng-yi-L <10722058+cheng-yi-l@user.noreply.gitee.com>
2026-06-01 19:20:57 +0800

    fix: 修复 gitignore 误忽略 SystemLogsView 并补充视图文件

---
+++ src/views/logs/SystemLogsView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '@/stores/auth'
+import SystemLogs from '@/components/SystemLogs.vue'
+
+const auth = useAuthStore()
+const { currentUser } = storeToRefs(auth)
+</script>
+
+<template>
+  <div v-if="currentUser" class="min-h-[70vh]">
+    <SystemLogs :current-user="currentUser" />
+  </div>
+</template>
